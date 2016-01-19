4c52d5268ef35104ee1f5935b89fff2357abeebe
Olivier Hagolle <olivier.hagolle@cnes.fr>
2016-01-19 22:38:03 +0100

    [ENH] extention to SpotWorldHeritage and add many options

--- theia_download.py
+++ theia_download.py
@@ -1,7 +1,7 @@
 import json
 import time
 import os, os.path, optparse,sys
-
+from datetime import date
 
 ###########################################################################
 class OptionParser (optparse.OptionParser):
@@ -23,9 +23,10 @@ if len(sys.argv) == 1:
     print '      '+sys.argv[0]+' [options]'
     print "     Aide : ", prog, " --help"
     print "        ou : ", prog, " -h"
-    print "example python  %s -q 'Toulouse 2014' -a auth_theia.txt"%sys.argv[0]
+    print "example : python %s -q 'Toulouse' -a hagolle_theia.txt -d 2015-11-01 -f 2015-12-01"%sys.argv[0]
+    print "example : python %s -q 'Toulouse' -a hagolle_theia.txt -c SpotWorldHeritage -p SPOT4 -d 2005-11-01 -f 2006-12-01"%sys.argv[0]
     sys.exit(-1)
-else:
+else :
     usage = "usage: %prog [options] "
     parser = OptionParser(usage=usage)
   
@@ -35,11 +36,25 @@ else:
             help="Theia account and password file")
     parser.add_option("-w","--write_dir", dest="write_dir", action="store",type="string",  \
             help="Path where the products should be downloaded",default='.')
-    parser.add_option("-c","--colelction", dest="collection", action="store",type="string",  \
-            help="Colection within theia collections",default='Landsat')
+    parser.add_option("-c","--collection", dest="collection", action="store", type="choice",  \
+            help="Collection within theia collections",choices=['Landsat','SpotWorldHeritage'],default='Landsat')
+    parser.add_option("-n","--no_download", dest="no_download", action="store_true",  \
+            help="Do not download products, just print curl command",default=False)
+    parser.add_option("-d", "--start_date", dest="start_date", action="store", type="string", \
+            help="start date, fmt('2015-12-22')",default=None)
+    parser.add_option("-f","--end_date", dest="end_date", action="store", type="string", \
+            help="end date, fmt('2015-12-23')",default=None)
+    parser.add_option('-p', '--platform', type='choice', action='store', dest='platform',\
+                      choices=['LANDSAT5','LANDSAT7','LANDSAT8','SPOT1','SPOT2','SPOT3','SPOT4','SPOT5'], default='LANDSAT8',  help='Satellite',)
 
     (options, args) = parser.parse_args()
 
+if options.start_date!=None:    
+    start_date=options.start_date
+    if options.end_date!=None:
+        end_date=options.end_date
+    else:
+        end_date=date.today().isoformat()
 
 
 #====================
@@ -54,7 +69,7 @@ try:
 except :
     print "error with password file"
     sys.exit(-2)
-query=options.query.replace(" ","%20")
+query=options.query.replace(" ","\ ")
 
 #============================================================
 # get a token to be allowed to bypass the authentification.
@@ -77,7 +92,7 @@ with open('token.json') as data_file:
 if os.path.exists('search.json'):
     os.remove('search.json')
     
-search_catalog='curl -k -o search.json "https://theia.cnes.fr/resto/api/collections/Landsat/search.json?q=%s\&maxRecords=500"'%query
+search_catalog='curl -k -o search.json https://theia.cnes.fr/resto/api/collections/%s/search.json?q=%s\&platform=%s\&startDate=%s\&completionDate=%s\&maxRecords=500'%(options.collection,query,options.platform,start_date,end_date)
 print search_catalog
 os.system(search_catalog)
 time.sleep(10)
@@ -95,5 +110,7 @@ for i in range(len(data["features"])):
     prod=data["features"][i]["properties"]["productIdentifier"]
     feature_id=data["features"][i]["id"]
     get_product='curl -o %s.zip -k -H "Authorization: Bearer %s" https://theia.cnes.fr/resto/collections/Landsat/%s/download/?issuerId=theia'%(prod,token,feature_id)
-    os.system(get_product)
+    print get_product
+    if not(options.no_download):
+        os.system(get_product)
 
